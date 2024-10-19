623c756e607807284cdb7ef5f09c9b0013e03804
FahithKRM <krmfahith@gmail.com>
2024-10-19 23:59:32 +0530

    first commit

---
+++ tem.js
@@ -0,0 +1,9 @@
+console.log("Random number : " + Math.floor(Math.random()*10+1))
+console.log("Random : " + Math.floor(Math.random()*10+1))
+
+
+var color = ["blue", "orange", "green", "red", "black", "purple", "rose", "gray"]
+
+for(i=0; i<color.length; i++){
+    console.log(color[i])
+}
